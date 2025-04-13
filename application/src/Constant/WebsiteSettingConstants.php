87e4f3d8468727620b61e005b3c0b114b305df57
jcalifice <jcalifice@outlook.com>
2025-04-13 21:44:24 +0200

    Cleanups

--- application/src/Constant/WebsiteSettingConstants.php
+++ application/src/Constant/WebsiteSettingConstants.php
@@ -4,7 +4,5 @@ namespace App\Constant;
 
 class WebsiteSettingConstants
 {
-    public const string ASSET_EVENT_DEFAULT_TEASER_IMAGE = 'eventDefaultTeaserImage';
-
-    public const string DOCUMENT_NAVIGATION_SERVICES_ROOT = 'navigationServicesRoot';
+    //public const string AN_EXAMPLE = 'myExample';
 }
