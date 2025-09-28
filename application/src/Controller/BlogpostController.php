7c44cad3d0a5d8a8d4c8cba87ff260426413c4e8
jcalifice <jcalifice@outlook.com>
2025-09-28 17:41:59 +0200

    Cleanup

--- application/src/Controller/BlogpostController.php
+++ application/src/Controller/BlogpostController.php
@@ -8,7 +8,6 @@ use App\Adapter\App\Database\Doctrine\Repository\TagRepository;
 use App\Service\BlogpostService;
 use App\Service\CollectionService;
 use Pimcore\Model\DataObject\Collection;
-use Pimcore\Model\DataObject\Tour;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -43,43 +42,5 @@ class BlogpostController extends BaseController
             'blogposts' => $blogposts ?? [],
             'tags' => $this->tagRepository->findAllOrderedByTagCategoryWeighting(),
         ]));
-    }
-
-    private function fetchFacts(): array
-    {
-        $facts = [];
-
-        // Tour facts
-        if ($this->tour) {
-            // Default facts
-            $facts['tour'] = [
-                'distance'    => (float) $this->tour->getTourFactDistance(),
-                'elevation'   => (int) $this->tour->getTourFactElevation(),
-                'duration'    => (float) $this->tour->getTourFactDuration(),
-                'pauses'      => (string) $this->tour->getTourFactPauses(),
-                'start'       => $this->tour->getTourStart(),
-                'startTitle'  => $this->tour->getTourStartTitle(),
-                'end'         => $this->tour->getTourEnd(),
-                'endTitle'    => $this->tour->getTourEndTitle(),
-                'orientation' => $this->tour->getTourFactOrientation(),
-            ];
-
-            // Brick : Hike
-            if ($this->tour->getTourAdditionalFacts()->getTourFactsHike()) {
-                $facts['tour'] = array_merge($facts['tour'], [
-                    'headForHeights' => $this->tour->getTourAdditionalFacts()->getTourFactsHike()->getTourFactHeadForHeights(),
-                    'surefootedness' => $this->tour->getTourAdditionalFacts()->getTourFactsHike()->getTourFactSurefootedness(),
-                ]);
-            }
-
-            // Brick : Bike
-            if ($this->tour->getTourAdditionalFacts()->getTourFactsBike()) {
-                $facts['tour'] = array_merge($facts['tour'], [
-                    'singletrailScale' => $this->tour->getTourAdditionalFacts()->getTourFactsBike()->getTourFactSingletrailScale(),
-                ]);
-            }
-        }
-
-        return $facts;
     }
 }
