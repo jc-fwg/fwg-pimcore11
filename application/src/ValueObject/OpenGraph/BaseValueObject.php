6cac2938026ce0f574af0b3a53f14c2c405c661b
jcalifice <jcalifice@outlook.com>
2025-11-11 17:25:04 +0100

    Added OG image width and height properties

--- application/src/ValueObject/OpenGraph/BaseValueObject.php
+++ application/src/ValueObject/OpenGraph/BaseValueObject.php
@@ -9,6 +9,8 @@ abstract readonly class BaseValueObject
     protected const string TYP_ARTICLE = 'article';
     protected const string TYP_WEBSITE = 'website';
     protected const string SITE_NAME   = 'FreiWeg Outdoor Activity Blog';
+    protected const string IMAGE_WIDTH   = '1200';
+    protected const string IMAGE_HEIGHT   = '630';
 
     public function __construct(
         public string $title,
@@ -17,6 +19,8 @@ abstract readonly class BaseValueObject
         public string $url,
         public string $type,
         public string $siteName = self::SITE_NAME,
+        public string $imageWidth = self::IMAGE_WIDTH,
+        public string $imageHeight = self::IMAGE_HEIGHT,
     ) {
     }
 }
